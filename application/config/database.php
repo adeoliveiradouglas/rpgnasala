4238e1f776f02b0295bc26c3f005e7d26ae569b1
Lima16 <32302575+Lima16@users.noreply.github.com>
2018-11-05 12:14:06 -0300

    Cadastro de Usuario - Criação do cadastro de usuário

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'pgsql:host=localhost;port=5432;dbname=rpgnasaladb',
-	'hostname' => '',
-	'username' => 'postgres',
-	'password' => '@4vx9qkm821',
-	'database' => '',
-	'dbdriver' => 'pdo',
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'rpgnasala',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
--- application/resources/PageConstants.php
+++ application/resources/PageConstants.php
@@ -4,6 +4,7 @@ class PageConstants {
     
     public const HOME = 'home';
     public const LOGIN = 'login';
+    public const USUARIO = 'usuario';
     
 }
 ?>
